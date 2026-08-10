3b05e10ae81a86c13fa64d7051c15f915a7a4891
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2026-08-10 18:26:02 +0200

    Update uninstall option prefix

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,7 @@
 <?php
 
-if(!defined('WP_UNINSTALL_PLUGIN')){
-
-exit;
-
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
 }
 
-
-delete_option(
-'aitn_settings'
-);
+delete_option('vtnai_settings');
